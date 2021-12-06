d709f2e3ced6a0e28b4df23920dd5d1a06a8dc60
Klepo313 <antonio.klepo1@gmail.com>
2021-12-06 11:25:33 +0100

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -24,4 +24,31 @@ const analytics = getAnalytics(app);
 var taskHeader = document.getElementById("taskHeader");
 
 var dbRef = firebase.database().ref()
-dbRef.on('value', snap => taskHeader.innerText = snap.val())
+dbRef.on('value', snap => taskHeader.innerText = snap.val())*/
+
+var taskName = document.getElementById("inputAddTask")
+var date = document.getElementById("inputPickADate");
+const btnAddTask = document.getElementById("btnAddTask");
+
+const task_list = document.getElementsByClassName("task-list")[0]
+
+var fas = ["fas fa-check", "fas fa-pen", "fas fa-trash-alt"]
+var ijevi = ["i1", "i2", "i3"]
+
+btnAddTask.addEventListener("click", function(){
+
+  let task = document.createElement("div")
+  task.classList.add("task")
+
+  let left_ctn = document.createElement("div")
+  let inpCheckbox = document.createElement("input")
+  inpCheckbox.setAttribute("type", "checkbox");
+  let task_content = document.createElement("div")
+  let taskHeader = document.createElement("span")
+  let taskDate = document.createElement("span")
+  let task_actions = document.createElement("div")
+  for(var i = 0; i < 3; i++){
+    ijevi[i] = document.createElement("i")
+    ijevi[i].classList.add(fas[i])
+  }
+})
