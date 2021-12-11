d3fe1835a54bfd175bb328ebad312cf7b8b717c4
TheKlepo75 <antonio.klepo1@gmail.com>
2021-12-11 12:26:16 +0100

    commit

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-// Import the functions you need from the SDKs you need
-/*
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAFmMd4Iur3GCLcO3-IWu8fzNUtGByCQJM",
-  authDomain: "to-do-live-8dd25.firebaseapp.com",
-  projectId: "to-do-live-8dd25",
-  storageBucket: "to-do-live-8dd25.appspot.com",
-  messagingSenderId: "1021120218423",
-  appId: "1:1021120218423:web:c16587b02867223e04c6b2",
-  measurementId: "${config.measurementId}"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
-
-*/
-
-/*JAVASCRIPT*/
+//npm install browserify-fs
 var taskName = document.getElementById("inputAddTask")
 var date = document.getElementById("inputPickADate")
 var btnAddTask = document.getElementById("btnAddTask");
 var submit = document.getElementsByClassName("fas")[0];
 
 var task = document.querySelector(".task")
+
+/*JAVASCRIPT*/
+
+fetch('./db.json')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    let task_list = document.querySelector(".task-list")
+    var count = Object.keys(data.tasks).length;
+
+    for(let i = 0; i < count; i++) {
+      let newTask = task.cloneNode(true)
+      task_list.appendChild(newTask);
+
+      newTask.querySelector("#taskHeader").value = data.tasks[i].ts_name
+      newTask.querySelector("#taskDate").value = data.tasks[i].ts_date
+    }
+
+})
 
 function addTaskFn(){
   let task_list = document.querySelector(".task-list")
@@ -40,16 +33,28 @@ function addTaskFn(){
     return false;
   }
 
-  var newTask = task.cloneNode(true);
+  let newTask = task.cloneNode(true)
   task_list.appendChild(newTask);
 
-  /*db.collection("todo-items").add({
-    text: taskName.value,
-    date: date.value
-  })*/
+  newTask.querySelector("#taskHeader").value = taskName.value
+  newTask.querySelector("#taskDate").value = date.value
 
-  newTask.querySelector("#taskHeader").value = taskName.value;
-  newTask.querySelector("#taskDate").value = date.value;
+  fetch('./db.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      var count = Object.keys(data.tasks).length;
+      var idCount = count + 1;
+
+      const newTaskDetails = {
+        "ts_id": idCount,
+        "ts_name": newTask.querySelector("#taskHeader").value,
+        "ts_date": newTask.querySelector("#taskDate").value
+      }
+
+    })
+    .catch((err) => console.log(err));
 }
 
 function deleteTask(button){
@@ -61,13 +66,15 @@ function deleteTask(button){
 
   }
 }
+
 function editTask(button){
   let task = button.parentNode.parentNode
   task.querySelector("#taskHeader").readOnly = false
   task.querySelector("#taskHeader").style.borderBottom = "1px solid white"
 }
-submit.addEventListener("click", function(){
+
+function submitChanges(button){
+  let task = button.parentNode.parentNode
   task.querySelector("#taskHeader").readOnly = true
   task.querySelector("#taskHeader").style.borderBottom = "none"
-})
-
+}
