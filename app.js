eb3f4c6ed06b707bb080687cb38ddb27f5f4647a
TheKlepo75 <antonio.klepo1@gmail.com>
2021-12-16 23:46:31 +0100

    commit

--- app.js
+++ app.js
@@ -67,10 +67,10 @@ function getDataFromLocalStorage(){
     let taskIzSt = JSON.parse(localStorage.getItem('todos'))
 
     if(taskIzSt){
-      let task_list = document.querySelector(".task-list")
-      let newTask = task.cloneNode(true)
-
       for(let i = 0; i < taskIzSt.length; i++) {
+        let task_list = document.querySelector(".task-list")
+        let newTask = task.cloneNode(true)
+
         newTask.querySelector("#taskHeader").value = taskIzSt[i].ts_name
         newTask.querySelector("#taskDate").value = taskIzSt[i].ts_date
 
@@ -88,8 +88,38 @@ function getDataFromLocalStorage(){
     }
 }
 
+function isChecked(input) {
+
+  let data = JSON.parse(localStorage.getItem('todos'));
+  let task = input.parentNode.parentNode
+  let task_header = task.querySelector("#taskHeader")
+
+  if (input.checked) {
+
+    for(let i=0; i<data.length; i++) {
+      if(data[i].ts_name == task_header.value){
+        data[i].completed = true;
+        break;
+      }
+    }
+    
+
+  } else{
+    
+    for(let i=0; i<data.length; i++) {
+      if(data[i].ts_name == task_header.value){
+        data[i].completed = false;
+        break;
+      }
+  }
+
+}
+
+localStorage.setItem("todos", JSON.stringify(data));
+
+}
+
 function addTodo(){
-  let taskIzSt = JSON.parse(window.localStorage.getItem('todos'))
   let task_list = document.querySelector(".task-list")
 
   if(taskName.value == null || taskName.value == "" || date.value == "" || date.value == null){
@@ -121,40 +151,59 @@ function addTodo(){
 
 }
 
-// function addToLocalStorage(todos) {
-//   localStorage.setItem('todos', JSON.stringify(todos));
-// }
-
-// function isCompleted(l_date) {
-//   todos.forEach((todo_item) => {
-//     if (todo_item.id == l_date) {
-//       todo_item.completed = !todo_item.completed;
-//     }
-//   })
-
-//   addToLocalStorage(todos);
-// }
-
 function deleteTask(button){
-  let task_list = button.parentNode.parentNode
+  let data = JSON.parse(localStorage.getItem('todos'));
+  let task = button.parentNode.parentNode
+  let task_header = task.querySelector("#taskHeader")
 
   if(confirm("Are you sure you want to delete this task?")){
-    task_list.parentNode.removeChild(task_list)
-    localStorage.removeItem('todos');
+
+    for(let i=0; i<data.length; i++) {
+      if(data[i].ts_name == task_header.value){
+        data.splice(i, 1);
+      }
+    }
+
+    task.parentNode.removeChild(task)
+    console.log("Data length: " + data.length)
+    
+    for(let i=0; i<data.length; i++) {
+      console.log(data[i])
+    }
+
+    localStorage.setItem("todos", JSON.stringify(data));
+
   } 
 
 }
 
+var temp_header = ''
+
 function editTask(button){
   let task = button.parentNode.parentNode
-  task.querySelector("#taskHeader").readOnly = false
-  task.querySelector("#taskHeader").style.borderBottom = "1px solid white"
+  let task_header = task.querySelector("#taskHeader")
+  temp_header = task_header.value
+
+  task_header.readOnly = false
+  task_header.style.borderBottom = "1px solid white"
 }
 
 function submitChanges(button){
+
+  let data = JSON.parse(localStorage.getItem('todos'));
   let task = button.parentNode.parentNode
-  task.querySelector("#taskHeader").readOnly = true
-  task.querySelector("#taskHeader").style.borderBottom = "none"
+  let task_header = task.querySelector("#taskHeader")
+  
+  task_header.readOnly = true
+  task_header.style.borderBottom = "none"
+
+  for(let i=0; i<data.length; i++) {
+    if(data[i].ts_name == temp_header){
+      data[i].ts_name = task_header.value
+      localStorage.setItem("todos", JSON.stringify(data));
+    }
+  }
+
 }
 
 function clearLocalStorage(){
