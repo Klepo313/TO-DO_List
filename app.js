7c6a6910b21ae6d1d6a56906214dafddbcec1f41
Klepo313 <antonio.klepo1@gmail.com>
2021-12-20 09:14:44 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,58 +1,4 @@
 
-
-/*var task = '<div class="task">' +
-                    '<div class="left-ctn">'+ 
-                        '<input type="checkbox" id="checkbox">'+
-                        '<div class="task-content">'+
-                            '<input type="text" value="Example Task" id="taskHeader" readonly>'+
-                            '<input type="text" value="2021-07-07" id="taskDate" readonly>'+
-                        '</div>'+
-                    '</div>'+
-                    '<div class="taskActions">'+
-                        '<i class="fas fa-check" onclick="submitChanges(this)"></i>'+
-                        '<i class="fas fa-pen" onclick="editTask(this)"></i>'+
-                        '<i class="fas fa-trash-alt" onclick="deleteTask(this)"></i>'+
-                    '</div>'+
-                '</div>'
-*/
-
-/*fetch('./db.json')
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let task_list = document.querySelector(".task-list")
-    var count = Object.keys(data.tasks).length;
-
-    for(let i = 0; i < count; i++) {
-      let newTask = task.cloneNode(true)
-      task_list.appendChild(newTask);
-
-      newTask.querySelector("#taskHeader").value = data.tasks[i].ts_name
-      newTask.querySelector("#taskDate").value = data.tasks[i].ts_date
-    }
-
-})*/
-
-/*fetch('./db.json')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      var count = Object.keys(data.tasks).length;
-      var idCount = count + 1;
-
-      const newTaskDetails = {
-        "ts_id": idCount,
-        "ts_name": newTask.querySelector("#taskHeader").value,
-        "ts_date": newTask.querySelector("#taskDate").value
-      }
-
-    })
-    .catch((err) => console.log(err));
-*/
-
-//npm install browserify-fs
 var taskName = document.getElementById("inputAddTask")
 var date = document.getElementById("inputPickADate")
 var btnAddTask = document.getElementById("btnAddTask");
@@ -82,9 +28,6 @@ function getDataFromLocalStorage(){
 
         task_list.appendChild(newTask);
       }
-        // newTask.querySelector("#taskHeader").value = todos[i].ts_name
-        // newTask.querySelector("#taskDate").value = todos[i].date
-        // newTask.querySelector("#checkbox").checked = todos[i].completed
     }
 }
 
